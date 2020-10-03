dae5190c8f23893cac645def81734641efe31912
Davidin <david.hernandezc@gmail.com>
2020-10-03 15:26:09 +0200

    con fichero de config

--- daping.py
+++ daping.py
@@ -9,11 +9,30 @@ from random import randrange
 from datetime import datetime
 from datetime import date
 import smtplib
+import configparser
+
+TESTEOS=0               #posicion en a lista en dic_rangos_contador
+EXITOSOS=1              #posicion en a lista en dic_rangos_contador
+EXITOSOS_SEGUIDOS=2     #posicion en a lista en dic_rangos_contador
+FALLIDOS=3              #posicion en a lista en dic_rangos_contador
+FALLIDOS_SEGUIDOS=4     #posicion en a lista en dic_rangos_contador
+LOG_CADA=1              #Cada cuando escupimos log al fichero log.txt
+BUSCA_IP_CADA=1         #Si un rango no tiene ips cada cuanto buscamos
+GUARDA_DIC_CADA=1       #Cada cuando salvamos el diccionario de rangos ppal
+MAIL_SI_FALLO=1         #Correo si falla un rango x veces seguidas
+MAIL_SI_RECUPERA=1      #Correo cuando recupera un rango x veces seguidas
+BUSCAIPS_SI_MENOS=10    #Busca IPs de nuevo si tiene menos de x
+MAXIMAS_IP_POR_RANGO=50 #Cuantas IPs guarda de cada rango como máximo
+MAXIMA_RED=21           #Si es más grande de X partimos la red en trozos como X
+TRUNC_IPS=50            #Recorta las IPs a  (solo aplica si hay más de x al incio al cargar el diccionario). Nunca inferior a 6
+ID=""
+MAILS=["tu@email1","tu@email2"] #Direcciones de envío de mail
 
 def envia_correo(asunto, mensaje):
     remitente = "david.hernandezc@gmail.com"
-    destinatario = "david.hernandez@vodafone.com"
-    asunto="DAPING: " + asunto
+    destinatario = MAILS
+    asunto="DAPING: " + ID + " "+ asunto
+    print(asunto)
     email = """From: %s
 To: %s
 MIME-Version: 1.0
@@ -24,7 +43,7 @@ Subject: %s
 """ % (remitente, destinatario, asunto, mensaje)
     try:
         smtp = smtplib.SMTP('localhost')
-        smtp.sendmail(remitente, destinatario, email)
+        smtp.sendmail(remitente, MAILS, email)
         print ("Correo enviado satisafactoriamente")
     except:
         print ("Error: el correo no ha sido enviado.")
@@ -56,10 +75,16 @@ def guarda_diccionario(dic,nombre_fichero):
     #print (dic)
 
 def carga_diccionario(nombre_fichero):
+    global TRUNC_IPS
     try:
         with open(nombre_fichero, "rb") as f:
             dic_cargado=pickle.load(f)
             print ("---------------Diccionario cargado---------------")
+            for rango in dic_cargado.keys(): #truncamos ips
+                if TRUNC_IPS<6:
+                    TRUNC_IPS=6 # como minimo debería ser 5 para no cargarnos el diccionario de contadores
+                if len(dic_cargado[rango])>TRUNC_IPS:
+                    dic_cargado[rango]=dic_cargado[rango][0:TRUNC_IPS]
             print(dic_cargado)
             return dic_cargado
     except (OSError, IOError) as e:
@@ -67,28 +92,73 @@ def carga_diccionario(nombre_fichero):
         return dict()
 
 def busca_ips_en_rango(rango):
-    MAXIMAS_IP_POR_RANGO=50 # Cuantas IPs guarda de cada rango como máximo
-    MAXIMA_RED=21   # Si es más grande de X partimos la red en trozos como X
+
     print ("---------------Buscando IPs en rango", rango)
     network=ipaddress.ip_network(rango)
     mascara=network.prefixlen
     if mascara > MAXIMA_RED-1:
         result = subprocess.run(["fping", "-gaq", str(rango)], capture_output=True, text=True)
-    else:  #Si el rango es <MAXIMA_RED seleccionamos uno de los MAXIMA_RED dentro de el
-        lista_subredes=list(network.subnets(new_prefix=MAXIMA_RED)) #Spliteamos en MAXIMA_RED
+    else:  #Si el rango es <max_red seleccionamos uno de los max_red dentro de el
+        lista_subredes=list(network.subnets(new_prefix=MAXIMA_RED)) #Spliteamos en max_red
         seleccionada=randrange(len(lista_subredes)) #Selecciona entre 0 y len-1
         print ("---------------Selecciono para ping la", seleccionada, "de",len(lista_subredes), str(lista_subredes[seleccionada]))
         result = subprocess.run(["fping", "-gaq", str(lista_subredes[seleccionada])], capture_output=True, text=True)
     lista_salida=result.stdout.splitlines() #result.stdout.splitlines es una lista de lineas con la salida del fping. Intentar limitar a 50 pings maximo, no tiene sentido tener mas.
     print("---------------Rango", rango, "procesado, ", len(lista_salida), "ips responden a ping")
-    #aqui podíamos truncar lista si no queremos tantas ips
     if len(lista_salida) > MAXIMAS_IP_POR_RANGO:
         lista_salida=lista_salida[0:MAXIMAS_IP_POR_RANGO]
     return (lista_salida)
 
+def carga_config():
+    global LOG_CADA
+    global BUSCA_IP_CADA
+    global GUARDA_DIC_CADA
+    global MAIL_SI_FALLO
+    global MAIL_SI_RECUPERA
+    global BUSCAIPS_SI_MENOS
+    global MAXIMAS_IP_POR_RANGO
+    global MAXIMA_RED
+    global TRUNC_IPS
+    global ID
+    global MAILS
+   
+    config = configparser.ConfigParser()
+    try:
+        with open ('daping.ini') as f:  #Falta gestionar si un id no existe en el fichero
+            config.read_file(f)
+            LOG_CADA=int(config['default']['LOG_CADA'])
+            BUSCA_IP_CADA=int(config['default']['BUSCA_IP_CADA'])
+            GUARDA_DIC_CADA=int(config['default']['GUARDA_DIC_CADA'])
+            MAIL_SI_FALLO=int(config['default']['MAIL_SI_FALLO'])
+            MAIL_SI_RECUPERA=int(config['default']['MAIL_SI_RECUPERA'])
+            BUSCAIPS_SI_MENOS=int(config['default']['BUSCAIPS_SI_MENOS'])
+            MAXIMAS_IP_POR_RANGO=int(config['default']['MAXIMAS_IP_POR_RANGO'])
+            MAXIMA_RED=int(config['default']['MAXIMA_RED'])
+            TRUNC_IPS=int(config['default']['TRUNC_IPS'])
+            ID=config['default']['ID']
+            MAILS=config['default']['MAILS']
+            print("Configuración cargada")
+            print (MAILS)
+    except (OSError, IOError) as e:
+        print ("No hay fichero configuración")
+
+def print_config():
+    config="\r\nConfiguración actual:\r\n"
+    config=config + "LOG_CADA: " + str(LOG_CADA) + "\r\n"
+    config=config + "BUSCA_IP_CADA: " + str(BUSCA_IP_CADA) + "\r\n"
+    config=config + "GUARDA_DIC_CADA: " + str(GUARDA_DIC_CADA) + "\r\n"
+    config=config + "MAIL_SI_FALLO: " + str(MAIL_SI_FALLO) + "\r\n"
+    config=config + "MAIL_SI_RECUPERA: " + str(MAIL_SI_RECUPERA) + "\r\n"
+    config=config + "BUSCAIPS_SI_MENOS: " + str(BUSCAIPS_SI_MENOS) + "\r\n"
+    config=config + "MAXIMAS_IP_POR_RANGO: " + str(MAXIMAS_IP_POR_RANGO) + "\r\n"
+    config=config + "MAXIMA_RED: " + str(MAXIMA_RED) + "\r\n"
+    config=config + "TRUNC_IPS: " + str(TRUNC_IPS) + "\r\n"
+    config=config + "ID: " + ID + "\r\n"
+    config=config + "MAILS: " + MAILS + "\r\n"
+    return (config)
 
 def main():
-    print ("Empezamos")
+    carga_config()
     fecha_inicio = date.today()
     dic_rangos_contador={}
     dic_rangos={} #Diccionario con rango (key) y lista de ips que responden (value)
@@ -120,25 +190,7 @@ def main():
     #Pendiente imprimir al log el dic y count  
     print ("---------------Entramos en modo control---------------------")
     #print(dic_rangos)
-
     # Ver si las IPs de un diccionario de rangos siguen respondiendo. Si una IP no responde la elimina de la lista
-
-    #dic_rangos["80.224.0.0/24"].append("80.224.0.1") #Añado una ip que no responde para que falle y probarla
-    #dic_rangos["80.224.0.0/24"].append("80.224.0.5") #Añado una ip que no responde para que falle y probarla
-    TESTEOS=0               #posicion en a lista en dic_rangos_contador
-    EXITOSOS=1              #posicion en a lista en dic_rangos_contador
-    EXITOSOS_SEGUIDOS=2     #posicion en a lista en dic_rangos_contador
-    FALLIDOS=3              #posicion en a lista en dic_rangos_contador
-    FALLIDOS_SEGUIDOS=4     #posicion en a lista en dic_rangos_contador
-    LOG_CADA=1              #Cada cuando escupimos log al fichero log.txt
-    BUSCA_IP_CADA=1         #Si un rango no tiene ips cada cuanto buscamos
-    GUARDA_DIC_CADA=1       #Cada cuando salvamos el diccionario de rangos ppal
-    MAIL_SI_FALLO=1         #Correo si falla un rango x veces seguidas
-    MAIL_SI_RECUPERA=1      #Correo cuando recupera un rango x veces seguidas
-    BUSCAIPS_SI_MENOS=10    #Busca IPs de nuevo si tiene menos de x
-
-    
-
     testeo=0
     
     while True:
@@ -147,25 +199,14 @@ def main():
         num_rangos=0
         num_ips_ping=0
         for rango in dic_rangos.keys():
-            #si queremos recortar las ips de ping
-            #dic_rangos[rango]=dic_rangos[rango][0:XX] no testeado∫
             network=ipaddress.ip_network(rango)
             num_ips+=network.num_addresses  
             num_rangos+=1
             num_ips_ping+=len(dic_rangos[rango])
-        if testeo % LOG_CADA == 0:
-            logfile2=open("ultimo.log", "w")
-            print ("------------------INICIO",datetime.now(),"------------------------", file=logfile2)
-            print ("Controlando", num_rangos, "rangos y",num_ips,"ips. Hacemos ping a",num_ips_ping,"ips.", file=logfile2)
-            logfile=open("log.txt", "a+") 
-            print ("------------------INICIO",datetime.now(),"------------------------", file=logfile)
-            print ("Controlando", num_rangos, "rangos y",num_ips,"ips. Hacemos ping a",num_ips_ping,"ips.", file=logfile)
-            fecha_actual=date.today() #Para mandar un correo una vez solo cuando cambiamos de día
-            if fecha_inicio.day != fecha_actual.day:
-                mailfile=open("mail.txt", "w") #pendente poner el dia mes y año al nombre fichero
-                print ("------------------INICIO",datetime.now(),"------------------------", file=mailfile)
-                print ("Controlando", num_rangos, "rangos y",num_ips,"ips. Hacemos ping a",num_ips_ping,"ips.", file=mailfile)
-             
+        logfile=open("ultimo.log", "w") 
+        print ("------------------INICIO",datetime.now(),"------------------------", file=logfile)
+        print ("Controlando", num_rangos, "rangos y",num_ips,"ips. Hacemos ping a",num_ips_ping,"ips.", file=logfile)
+        fecha_actual=date.today() #Para mandar un correo una vez solo cuando cambiamos de día
         print("------------------------","Testeo numero", testeo, "-----------------------")
         print ("Controlando", num_rangos, "rangos y",num_ips,"ips. Hacemos ping a",num_ips_ping,"ips.")
         for rango in dic_rangos.keys():     # Por cada rango en el diccionario
@@ -196,22 +237,14 @@ def main():
                     dic_rangos_contador[rango][EXITOSOS_SEGUIDOS]=0
                 dic_rangos_contador[rango][TESTEOS]+=1
                 print(rango, len(dic_rangos[rango]),"IPs responden al ping", "T E ES F FS", dic_rangos_contador[rango])
-                if testeo % LOG_CADA == 0: #guardamos log de como vamos
-                    print(rango, len(dic_rangos[rango]),"IPs responden al ping", "T E ES F FS", dic_rangos_contador[rango], file=logfile)
-                    print(rango, len(dic_rangos[rango]),"IPs responden al ping", "T E ES F FS", dic_rangos_contador[rango], file=logfile2)      
-                    if fecha_inicio.day != fecha_actual.day:
-                        print(rango, len(dic_rangos[rango]),"IPs responden al ping", "T E ES F FS", dic_rangos_contador[rango], file=mailfile)           
+                print(rango, len(dic_rangos[rango]),"IPs responden al ping", "T E ES F FS", dic_rangos_contador[rango], file=logfile)
             else:
                 dic_rangos_contador[rango][FALLIDOS]+=1
                 dic_rangos_contador[rango][FALLIDOS_SEGUIDOS]+=1
                 dic_rangos_contador[rango][EXITOSOS_SEGUIDOS]=0
                 dic_rangos_contador[rango][TESTEOS]+=1
                 print ("**********ALERTA", rango, "SIN IP", "T E ES F FS", dic_rangos_contador[rango])
-                if testeo % LOG_CADA == 0: #guardamos log de como vamos
-                    print ("**********ALERTA", rango, "SIN IP", "T E ES F FS", dic_rangos_contador[rango], file=logfile)
-                    print ("**********ALERTA", rango, "SIN IP", "T E ES F FS", dic_rangos_contador[rango], file=logfile2)
-                    if fecha_inicio.day != fecha_actual.day:
-                        print ("**********ALERTA", rango, "SIN IP", "T E ES F FS", dic_rangos_contador[rango], file=mailfile)
+                print ("**********ALERTA", rango, "SIN IP", "T E ES F FS", dic_rangos_contador[rango], file=logfile)
                 if testeo % BUSCA_IP_CADA == 0: #Testeamos rangos con 0
                     dic_rangos[rango]=busca_ips_en_rango(rango)
             if dic_rangos_contador[rango][FALLIDOS_SEGUIDOS] == MAIL_SI_FALLO:
@@ -223,29 +256,30 @@ def main():
                     texto="Recuperacion en rango " + rango + " T E ES F FS " + str(dic_rangos_contador[rango])
                     print (texto)
                     envia_correo(texto, texto)
-        if testeo % LOG_CADA == 0:
-            print ("------------------FIN",datetime.now(),"------------------------", file=logfile)
-            print ("------------------FIN",datetime.now(),"------------------------", file=logfile2)
-            logfile.close()
-            logfile2.close()
-            logfile2=open("ultimo.log", "r")
-            logfile3=open("ultimo.txt", "w")
-            for linea in logfile2:
-                print (linea[:-1], file=logfile3)
-            logfile2.close()
-            logfile3.close()
-            if fecha_inicio.day != fecha_actual.day:
-                print ("------------------FIN",datetime.now(),"------------------------", file=mailfile)
-                mailfile.close()
-                mailfile=open("mail.txt", "r")
-                texto=""
-                for linea_log in mailfile:
-                    texto=texto+linea_log+ "<br>"
-                mailfile.close()
-                envia_correo("Reporte diario PINGs", texto)
-                fecha_inicio=date.today()
-
-        
+        print ("------------------FIN",datetime.now(),"------------------------", file=logfile)
+        logfile.close()
+        logfile2=open("ultimo.log", "r")
+        logfile3=open("ultimo.txt", "w") # lo copiamos a .txt pq el .log al estar abierto siempre no tiene toda la info
+        for linea in logfile2:
+            print (linea[:-1], file=logfile3)
+        logfile2.close()
+        print (print_config(), file=logfile3)
+        logfile3.close()
+        if fecha_inicio.day != fecha_actual.day:
+            mailfile=open("ultimo.txt", "r")
+            texto=""
+            for linea_log in mailfile:
+                texto=texto+linea_log+ "<br>"
+            envia_correo("Reporte diario PINGs", texto)
+            fecha_inicio=date.today()
+            mailfile.close()
+        if testeo % LOG_CADA ==0:
+            logfile4=open("daping.log","a+")
+            logfile5=open("ultimo.log", "r")
+            for linea_log in logfile5:
+                print (linea_log[:-1], file=logfile4)
+            logfile4.close()
+            logfile5.close()
         if testeo % GUARDA_DIC_CADA == 0: #Guardamos diccionario
             guarda_diccionario(dic_rangos,"dic_rangos.dat")
             guarda_diccionario(dic_rangos_contador,"dic_rangos_cont.dat")
