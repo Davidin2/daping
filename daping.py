a8bc77e8cc05b7294c67f8d93ed7e13d856e7e10
Davidin <david.hernandezc@gmail.com>
2020-10-07 12:25:24 +0200

    Mejora formato HTML, nuevos contadores 24horas

--- daping.py
+++ daping.py
@@ -1,4 +1,4 @@
-# Programado por David Hernandez david.hernandezc@gmail.com
+# Programmed by David Hernandez david.hernandezc@gmail.com year 2020
 # https://github.com/Davidin2/dapings
 
 
@@ -16,6 +16,11 @@ EXITOSOS=1              #posicion en a lista en dic_rangos_contador
 EXITOSOS_SEGUIDOS=2     #posicion en a lista en dic_rangos_contador
 FALLIDOS=3              #posicion en a lista en dic_rangos_contador
 FALLIDOS_SEGUIDOS=4     #posicion en a lista en dic_rangos_contador
+TESTEOS_24=5            #posicion en a lista en dic_rangos_contador
+EXITOSOS_24=6           #posicion en a lista en dic_rangos_contador
+EXITOSOS_SEGUIDOS_24=7  #posicion en a lista en dic_rangos_contador
+FALLIDOS_24=8           #posicion en a lista en dic_rangos_contador
+FALLIDOS_SEGUIDOS_24=9  #posicion en a lista en dic_rangos_contador
 LOG_CADA=1              #Cada cuando escupimos log al fichero log.txt
 BUSCA_IP_CADA=1         #Si un rango no tiene ips cada cuanto buscamos
 GUARDA_DIC_CADA=1       #Cada cuando salvamos el diccionario de rangos ppal
@@ -25,7 +30,7 @@ BUSCAIPS_SI_MENOS=10    #Busca IPs de nuevo si tiene menos de x
 MAXIMAS_IP_POR_RANGO=50 #Cuantas IPs guarda de cada rango como máximo
 MAXIMA_RED=21           #Si es más grande de X partimos la red en trozos como X
 TRUNC_IPS=50            #Recorta las IPs a  (solo aplica si hay más de x al incio al cargar el diccionario). Nunca inferior a 6
-ID=""
+ID=""                   #Para diferenciar si tienes varias instancias corriendo
 MAILS=["tu@email1","tu@email2"] #Direcciones de envío de mail
 
 def envia_correo(asunto, mensaje):
@@ -44,34 +49,34 @@ Subject: %s
     try:
         smtp = smtplib.SMTP('localhost')
         smtp.sendmail(remitente, MAILS, email)
-        print ("Correo enviado satisafactoriamente")
+        print ("Email sent succesfully")
     except:
-        print ("Error: el correo no ha sido enviado.")
+        print ("Error: we canot send the email")
 
 
 def carga_rangos(fichero):
     try:
         with open(fichero, "r") as f:
             lista_rangos=[]
-            print ("---------------Cargamos rangos de",fichero,"---------------")
+            print ("---------------Load ranges from",fichero,"---------------")
             for linea in f:
                 try:
                     ip = ipaddress.IPv4Network(linea[:-1]) # para quitar el retorno de carro
-                    print(ip, "es una red correcta")
+                    print(ip, "it is a correct network")
                     lista_rangos.append(linea[:-1]) 
                 except ValueError:
-                    print(linea, "es una red incorrecta y no se cargara")
-            print ("---------------Rangos cargados---------------")
+                    print(linea, "it is a incorrect network. Not loading")
+            print ("---------------Loaded Ranges---------------")
             print(lista_rangos)
             return lista_rangos
     except (OSError, IOError) as e:
-        print ("---------------No hay rangos a cargar---------------")
+        print ("---------------No ranges to load---------------")
         return list()   
 
 def guarda_diccionario(dic,nombre_fichero):
     with open(nombre_fichero, "wb") as f:
         pickle.dump(dic, f)
-    print("---------------Diccionario guardado---------------")
+    print("---------------Dicc saved---------------")
     #print (dic)
 
 def carga_diccionario(nombre_fichero):
@@ -79,7 +84,7 @@ def carga_diccionario(nombre_fichero):
     try:
         with open(nombre_fichero, "rb") as f:
             dic_cargado=pickle.load(f)
-            print ("---------------Diccionario cargado---------------")
+            print ("---------------Loaded Dicc---------------")
             for rango in dic_cargado.keys(): #truncamos ips
                 if TRUNC_IPS<6:
                     TRUNC_IPS=6 # como minimo debería ser 5 para no cargarnos el diccionario de contadores
@@ -88,12 +93,12 @@ def carga_diccionario(nombre_fichero):
             #print(dic_cargado)
             return dic_cargado
     except (OSError, IOError) as e:
-        print ("---------------No hay Diccionario a cargar---------------")
+        print ("---------------No Dicc to load---------------")
         return dict()
 
 def busca_ips_en_rango(rango):
 
-    print ("---------------Buscando IPs en rango", rango)
+    print ("---------------Searching IPs in range", rango)
     network=ipaddress.ip_network(rango)
     mascara=network.prefixlen
     if mascara > MAXIMA_RED-1:
@@ -101,10 +106,10 @@ def busca_ips_en_rango(rango):
     else:  #Si el rango es <max_red seleccionamos uno de los max_red dentro de el
         lista_subredes=list(network.subnets(new_prefix=MAXIMA_RED)) #Spliteamos en max_red
         seleccionada=randrange(len(lista_subredes)) #Selecciona entre 0 y len-1
-        print ("---------------Selecciono para ping la", seleccionada, "de",len(lista_subredes), str(lista_subredes[seleccionada]))
+        print ("---------------Select to ping", seleccionada, "of",len(lista_subredes), str(lista_subredes[seleccionada]))
         result = subprocess.run(["fping", "-gaq", str(lista_subredes[seleccionada])], capture_output=True, text=True)
     lista_salida=result.stdout.splitlines() #result.stdout.splitlines es una lista de lineas con la salida del fping. Intentar limitar a 50 pings maximo, no tiene sentido tener mas.
-    print("---------------Rango", rango, "procesado, ", len(lista_salida), "ips responden a ping")
+    print("---------------Range", rango, "processed, ", len(lista_salida), "ips answer ping")
     if len(lista_salida) > MAXIMAS_IP_POR_RANGO:
         lista_salida=lista_salida[0:MAXIMAS_IP_POR_RANGO]
     return (lista_salida)
@@ -148,23 +153,23 @@ def carga_config():
                 ID=config['default']['ID']
             if 'MAILS' in config['default']:
                 MAILS=config['default']['MAILS'].split(sep=',')
-            print("Configuracion cargada")
+            print("Configuration loaded")
     except (OSError, IOError) as e:
-        print ("No hay fichero configuración")
+        print ("No configuration file")
 
 def print_config():
-    config="\r\nConfiguración actual:\r\n"
-    config=config + "LOG_CADA: " + str(LOG_CADA) + "\r\n"
-    config=config + "BUSCA_IP_CADA: " + str(BUSCA_IP_CADA) + "\r\n"
-    config=config + "GUARDA_DIC_CADA: " + str(GUARDA_DIC_CADA) + "\r\n"
-    config=config + "MAIL_SI_FALLO: " + str(MAIL_SI_FALLO) + "\r\n"
-    config=config + "MAIL_SI_RECUPERA: " + str(MAIL_SI_RECUPERA) + "\r\n"
-    config=config + "BUSCAIPS_SI_MENOS: " + str(BUSCAIPS_SI_MENOS) + "\r\n"
-    config=config + "MAXIMAS_IP_POR_RANGO: " + str(MAXIMAS_IP_POR_RANGO) + "\r\n"
-    config=config + "MAXIMA_RED: " + str(MAXIMA_RED) + "\r\n"
-    config=config + "TRUNC_IPS: " + str(TRUNC_IPS) + "\r\n"
-    config=config + "ID: " + ID + "\r\n"
-    config=config + "MAILS: " + ",".join(MAILS) + "\r\n"
+    config="\r\nCurrent configuration:\r\n<BR>"
+    config=config + "Log to file every x pass: " + str(LOG_CADA) + "\r\n<BR>"
+    config=config + "Search IPs every x pass: " + str(BUSCA_IP_CADA) + "\r\n<BR>"
+    config=config + "Save dicc every x pass: " + str(GUARDA_DIC_CADA) + "\r\n<BR>"
+    config=config + "EMAIL if range fail: " + str(MAIL_SI_FALLO) + "\r\n<BR>"
+    config=config + "EMAIL if range recover: " + str(MAIL_SI_RECUPERA) + "\r\n<BR>"
+    config=config + "Search IP if less than: " + str(BUSCAIPS_SI_MENOS) + "\r\n<BR>"
+    config=config + "MAX IP to ping in a range: " + str(MAXIMAS_IP_POR_RANGO) + "\r\n<BR>"
+    config=config + "MAX prefix network to search IP: " + str(MAXIMA_RED) + "\r\n<BR>"
+    config=config + "Lmit IPs in range to: " + str(TRUNC_IPS) + "\r\n<BR>"
+    config=config + "ID: " + ID + "\r\n<BR>"
+    config=config + "EMAILS: " + ",".join(MAILS) + "\r\n<BR>"
     return (config)
 
 def main():
@@ -176,17 +181,17 @@ def main():
     dic_rangos_contador=carga_diccionario("dic_rangos_cont.dat")
     if dic_rangos_contador =={}: # si no hay fichero de contador lo inicializamos
         for rango in dic_rangos.keys():
-            dic_rangos_contador[rango]=[0,0,0,0,0]  # testeos, exitosos, fallidos, ver constantes
+            dic_rangos_contador[rango]=[0,0,0,0,0,0,0,0,0,0]  # testeos, exitosos, fallidos, ver constantes
 
     rangos=carga_rangos("nuevos_rangos.txt")
         #Buscar IPs en Rangos y rellenamos diccionario:
     for rango in rangos:      #En cada rango de la lista
         if rango not in dic_rangos: # Añadimos rango si no existe
             dic_rangos[rango]=busca_ips_en_rango(rango)  #Añadimos al diccionario las ips que responden       
-            dic_rangos_contador[rango]=[0,0,0,0,0]
-            print(rango, "cargado ")
+            dic_rangos_contador[rango]=[0,0,0,0,0,0,0,0,0,0]
+            print(rango, "loaded ")
         else:
-            print(rango, "no cargado porque ya estaba")
+            print(rango, "not loaded, already in tool")
         #Borramos rangos desde fichero
     rangos_a_borrar=carga_rangos("borrar_rangos.txt")
     for rango in rangos_a_borrar:      #En cada rango de la lista
@@ -194,11 +199,11 @@ def main():
             #lo borramos
             del (dic_rangos[rango])
             del (dic_rangos_contador[rango])
-            print(rango, "borrado ")
+            print(rango, "deleted ")
         else:
-            print(rango, "No se puede borrar pq no está")
+            print(rango, "not deleted, range not in tool")
     #Pendiente imprimir al log el dic y count  
-    print ("---------------Entramos en modo control---------------------")
+    print ("---------------Going in control mode---------------------")
     #print(dic_rangos)
     # Ver si las IPs de un diccionario de rangos siguen respondiendo. Si una IP no responde la elimina de la lista
     testeo=0
@@ -214,16 +219,16 @@ def main():
             num_rangos+=1
             num_ips_ping+=len(dic_rangos[rango])
         logfile=open("ultimo.log", "w") 
-        print ("------------------INICIO",datetime.now(),"------------------------", file=logfile)
-        print ("Controlando", num_rangos, "rangos y",num_ips,"ips. Hacemos ping a",num_ips_ping,"ips.", file=logfile)
+        print ("------------------Start time",datetime.now(),"------------------------<BR>", file=logfile)
+        print ("Checking", num_rangos, "ranges and",num_ips,"ips. Ping to",num_ips_ping,"""ips.<BR><TABLE BORDER="1">""", file=logfile)
         fecha_actual=date.today() #Para mandar un correo una vez solo cuando cambiamos de día
-        print("------------------------","Testeo numero", testeo, "-----------------------")
-        print ("Controlando", num_rangos, "rangos y",num_ips,"ips. Hacemos ping a",num_ips_ping,"ips.")
+        print("------------------------","Testing number", testeo, "-----------------------")
+        print ("Checking", num_rangos, "ranges and",num_ips,"ips. Ping to",num_ips_ping,"ips.")
         for rango in dic_rangos.keys():     # Por cada rango en el diccionario
             if len(dic_rangos[rango])>0:    # Si tiene al menos una ip que responde
                 if len(dic_rangos[rango])<BUSCAIPS_SI_MENOS: # si tiene menos del limite 
                     if testeo % BUSCA_IP_CADA == 0: #Y además toca buscar
-                        print ("Como solo tiene",len(dic_rangos[rango]),"ip, buscamos mas)")
+                        print ("Range",rango ,"with only",len(dic_rangos[rango]),"ip, searching more ips)")
                         dic_rangos[rango]+=busca_ips_en_rango(rango)
                         #podríamos quitar ips duplicadas, aunque puede ser interesante no hacerlo para que si no encuentra más no se quede siempre buscando
                 comando= ["fping", "-aq"]
@@ -241,32 +246,43 @@ def main():
                     dic_rangos_contador[rango][EXITOSOS]+=1
                     dic_rangos_contador[rango][EXITOSOS_SEGUIDOS]+=1
                     dic_rangos_contador[rango][FALLIDOS_SEGUIDOS]=0
+                    dic_rangos_contador[rango][EXITOSOS_24]+=1
+                    dic_rangos_contador[rango][EXITOSOS_SEGUIDOS_24]+=1
+                    dic_rangos_contador[rango][FALLIDOS_SEGUIDOS_24]=0
                 else:
                     dic_rangos_contador[rango][FALLIDOS]+=1
                     dic_rangos_contador[rango][FALLIDOS_SEGUIDOS]+=1
                     dic_rangos_contador[rango][EXITOSOS_SEGUIDOS]=0
+                    dic_rangos_contador[rango][FALLIDOS_24]+=1
+                    dic_rangos_contador[rango][FALLIDOS_SEGUIDOS_24]+=1
+                    dic_rangos_contador[rango][EXITOSOS_SEGUIDOS_24]=0
                 dic_rangos_contador[rango][TESTEOS]+=1
-                print(rango, len(dic_rangos[rango]),"IPs responden al ping", "T E ES F FS", dic_rangos_contador[rango])
-                print(rango, len(dic_rangos[rango]),"IPs responden al ping", "T E ES F FS", dic_rangos_contador[rango], file=logfile)
+                dic_rangos_contador[rango][TESTEOS_24]+=1
+                print(rango, len(dic_rangos[rango]),"IPs answer ping.", "[Tests TOTAL_OK CONSECUTIVE_OK FAIL CONSECUTIVE_FAIL]", dic_rangos_contador[rango][0:5],"Last 24h:", dic_rangos_contador[rango][5:10])
+                print("<TR><TD>", rango, len(dic_rangos[rango]),"IPs answer ping</TD>", "<TD>[Tests TOTAL_OK CONSECUTIVE_OK FAIL CONSECUTIVE_FAIL]", dic_rangos_contador[rango][0:5],"Last 24h:", dic_rangos_contador[rango][5:10], "</TD></TR>", file=logfile)
             else:
                 dic_rangos_contador[rango][FALLIDOS]+=1
                 dic_rangos_contador[rango][FALLIDOS_SEGUIDOS]+=1
                 dic_rangos_contador[rango][EXITOSOS_SEGUIDOS]=0
                 dic_rangos_contador[rango][TESTEOS]+=1
-                print ("**********ALERTA", rango, "SIN IP", "T E ES F FS", dic_rangos_contador[rango])
-                print ("**********ALERTA", rango, "SIN IP", "T E ES F FS", dic_rangos_contador[rango], file=logfile)
+                dic_rangos_contador[rango][FALLIDOS_24]+=1
+                dic_rangos_contador[rango][FALLIDOS_SEGUIDOS_24]+=1
+                dic_rangos_contador[rango][EXITOSOS_SEGUIDOS_24]=0
+                dic_rangos_contador[rango][TESTEOS_24]+=1
+                print ("**********ALERT", rango, "WITHOUT IP.", "[Tests TOTAL_OK CONSECUTIVE_OK FAIL CONSECUTIVE_FAIL]", dic_rangos_contador[rango][0:5],"Last 24h:", dic_rangos_contador[rango][5:10])
+                print ("<TR><TD>**********ALERT", rango, "WITHOUT IP</TD>", "<TD>[Tests TOTAL_OK CONSECUTIVE_OK FAIL CONSECUTIVE_FAIL]", dic_rangos_contador[rango][0:5],"Last 24h:", dic_rangos_contador[rango][5:10],"</TD></TR>", file=logfile)
                 if testeo % BUSCA_IP_CADA == 0: #Testeamos rangos con 0
                     dic_rangos[rango]=busca_ips_en_rango(rango)
             if dic_rangos_contador[rango][FALLIDOS_SEGUIDOS] == MAIL_SI_FALLO:
-                texto="FALLO en rango " + rango + " T E ES F FS " + str(dic_rangos_contador[rango])
+                texto="FAIL in range " + rango + ". [Tests TOTAL_OK CONSECUTIVE_OK FAIL CONSECUTIVE_FAIL] " + str(dic_rangos_contador[rango][0:5]) + " Last 24h: "+ str(dic_rangos_contador[rango][5:10])
                 print (texto)
                 envia_correo(texto, texto)
             if dic_rangos_contador[rango][EXITOSOS_SEGUIDOS] == MAIL_SI_RECUPERA: 
                 if dic_rangos_contador[rango][EXITOSOS_SEGUIDOS] != dic_rangos_contador[rango][TESTEOS]: # controlamos que no sea al arranque
-                    texto="Recuperacion en rango " + rango + " T E ES F FS " + str(dic_rangos_contador[rango])
+                    texto="Recover range " + rango + ". [Tests TOTAL_OK CONSECUTIVE_OK FAIL CONSECUTIVE_FAIL] " + str(dic_rangos_contador[rango][0:5])+" Last 24h: "+ str(dic_rangos_contador[rango][5:10])
                     print (texto)
                     envia_correo(texto, texto)
-        print ("------------------FIN",datetime.now(),"------------------------", file=logfile)
+        print ("</TABLE>------------------END TIME",datetime.now(),"------------------------<BR>", file=logfile)
         logfile.close()
         logfile2=open("ultimo.log", "r")
         logfile3=open("ultimo.txt", "w") # lo copiamos a .txt pq el .log al estar abierto siempre no tiene toda la info
@@ -279,15 +295,20 @@ def main():
             mailfile=open("ultimo.txt", "r")
             texto=""
             for linea_log in mailfile:
-                texto=texto+linea_log+ "<br>"
-            envia_correo("Reporte diario PINGs", texto)
+                texto=texto+linea_log
+            envia_correo("Daily Ping Report", texto)
             fecha_inicio=date.today()
             mailfile.close()
+            dic_rangos_contador[rango][FALLIDOS_24]=0
+            dic_rangos_contador[rango][FALLIDOS_SEGUIDOS_24]=0
+            dic_rangos_contador[rango][EXITOSOS_SEGUIDOS_24]=0
+            dic_rangos_contador[rango][EXITOSOS_24]=0
+            dic_rangos_contador[rango][TESTEOS_24]=0
         if testeo % LOG_CADA ==0:
             logfile4=open("daping.log","a+")
             logfile5=open("ultimo.log", "r")
             for linea_log in logfile5:
-                print (linea_log[:-1], file=logfile4)
+                print (linea_log[:-1], file=logfile4)  #Habría que limpiar el codigo htlml
             logfile4.close()
             logfile5.close()
         if testeo % GUARDA_DIC_CADA == 0: #Guardamos diccionario
